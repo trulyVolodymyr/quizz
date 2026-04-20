fda7534388fc24b0bba734f6c7db5f9a93c86839
Vychikov <v.vychikov@softonix.org>
2026-04-20 19:51:28 +0300

    configure development server to run on port 3000 with strict port enforcement

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/quizz/',
   plugins: [vue()],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       vue: 'vue/dist/vue.runtime.esm-bundler.js',
