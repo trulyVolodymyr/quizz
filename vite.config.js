d7bbfb150b8279a542cf7acbe4d72f7828bf57ec
Vychikov <v.vychikov@softonix.org>
2026-04-20 19:44:11 +0300

    use vue runtime-only build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/quizz/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.runtime.esm-bundler.js',
+    },
+  },
 })
